c941dea2195b660023d2e99e8f7174decd36a743
NewPaulWalker <linzhida19@mails.ucas.ac.cn>
2021-08-12 17:13:20 +0800

    judge a number is a prime

--- UsefulBlock/isprime.c
+++ UsefulBlock/isprime.c
@@ -1,7 +1,8 @@
-int isprime(int x){
+int isprime(int n){
+    //判断数字n是不是素数
     int i;
-    for(i=2;i*i<=x;i++){
-        if(x%i == 0)
+    for(i=2;i*i<=n;i++){
+        if(n%i == 0)
             return 0;
     }
     return 1;
