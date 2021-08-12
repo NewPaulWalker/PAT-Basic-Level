423f4adf6277c2cc527785315204f0cb60d67f63
NewPaulWalker <linzhida19@mails.ucas.ac.cn>
2021-08-12 18:35:23 +0800

    1010DerivationOfUnivariatePolynomial V0

---
+++ 1010DerivationOfUnivariatePolynomial.c
@@ -0,0 +1,33 @@
+//一元多项式求导
+#include<stdio.h>
+#define MAX 100
+int main(){
+    int a[MAX]; //系数
+    int e[MAX]; //指数
+    //输入
+    int i = 0;
+    int x;
+    do{
+        scanf("%d %d",&a[i],&e[i]);
+        i++;
+        x = getchar();
+    }while(x!='\n');
+    int len = i;    //项数
+    int f = 0;      //辅助输出格式
+    for(i=0;i<len;i++){
+        if(a[i]==0 && e[i]==0){
+            printf("0 0\n");
+            return 0;
+        }else{
+            if(e[i]){
+                if(f++==0){
+                    printf("%d %d",a[i]*e[i],e[i]-1);
+                }else{
+                    printf(" %d %d",a[i]*e[i],e[i]-1);
+                }
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
